dac040efc68ce4472dabd2de226f0862d3c426d2
fauvel <fauvel@gmx.de>
2012-12-09 13:01:22 +0100

    Updated address and added link to GitHub on the webpage.
    Change-Id: I94953216cf3c2921391bc95f71656ffd5895c8f6

--- index.php
+++ index.php
@@ -66,7 +66,8 @@
 					<img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" />
 				</a>
 				<br />
-				<span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">Quantum Ethics</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://ens.academia.edu/S%C3%A9bastienFauvel" property="cc:attributionName" rel="cc:attributionURL">Sébastien Fauvel</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.
+				<span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">Quantum Ethics</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://ens.academia.edu/S%C3%A9bastienFauvel" property="cc:attributionName" rel="cc:attributionURL">Sébastien Fauvel</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.<br />
+				<span>Source code available on <a href="https://github.com/fauvel/Quantum-Ethics">GitHub</a> for collaboration.</span>
 			</div>
 		</div>
 	</body>
